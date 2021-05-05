b82a2fab22aed623a29002f7d32a6fc35fefd47c
bipasha39 <physiobipasha39@gmail.com>
2021-05-04 19:29:32 -0700

    step 5

--- src/index.js
+++ src/index.js
@@ -3,18 +3,38 @@ const fetch = require("node-fetch");
 fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g")
   .then((response) => response.json())
   .then((data) => {
+    //parseDrinks into better formate
+    const parsedDrinks = data.drinks.map(parseDrink)
+
     // filter drinks which have four or more ingredients
-    const fancyDrinks = data.drinks.filter((drink) => {
-      if (drink.strIngredient4 != null) return true;
+    const fancyDrinks = parsedDrinks.filter((drink) => {
+      if (drink.ingredients.length > 3) return true;
     });
+
     // Log the total number of object return
     console.log("total number of drinks are", fancyDrinks.length);
     // For each cocktail object (console.log) its name
 
-    console.log(
-      "All names are",
-      fancyDrinks.map((drink) => drink.strDrink)
-    );
+   fancyDrinks.forEach(drink => console.log("Drink Id",drink.id,"is called",drink.name,"and is made with",drink.ingredients))
   })
 
   .catch((e) => console.error(e));
+
+  //parseDrink function takes a drink object and returns a new object with a id ,name and ingredients
+  const parseDrink=(drink)=>{
+    return {
+      id: drink.idDrink,
+      name:drink.strDrink,
+      ingredients: parseIngredients(drink)
+    }
+  }
+// parse ingredient function takes a drink object and return an array of ingredient name
+  const parseIngredients =(drink)=>{
+    const array = []
+    let i = 1
+    while (drink['strIngredient'+ i]!= null){
+      array.push(drink['strIngredient'+ i])
+      i++
+    }
+    return array ;
+  }

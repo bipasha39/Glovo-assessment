a6577ba7d8e5dccd86a58ceaeefc42b58b149f2f
bipasha39 <physiobipasha39@gmail.com>
2021-05-04 21:12:28 -0700

    step 6 and 7

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g")
   .then((response) => response.json())
   .then((data) => {
     //parseDrinks into better formate
-    const parsedDrinks = data.drinks.map(parseDrink)
+    const parsedDrinks = data.drinks.map(parseDrink);
 
     // filter drinks which have four or more ingredients
     const fancyDrinks = parsedDrinks.filter((drink) => {
@@ -12,44 +12,53 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=g")
     });
 
     // Log the total number of object return
-    console.log("total number of drinks are", fancyDrinks.length);
+    console.log("Total number of drinks are", fancyDrinks.length);
     // For each cocktail object (console.log) its name
-  
-    const alcholicdrinks = fancyDrinks.filter(drink=> drink.strAlcoholic == "Alcoholic")
-   console.log("Alcholic drinks are")
-    alcholicdrinks.forEach(logDrink)
 
-    const nonAlcholicdrinks = fancyDrinks.filter(drink=> drink.strAlcoholic != "Alcoholic")
-   console.log("Nonalcholic drinks are")
-    nonAlcholicdrinks.forEach(logDrink)
+    const alcholicdrinks = fancyDrinks.filter(
+      (drink) => drink.strAlcoholic == "Alcoholic"
+    );
+    console.log("Alcholic drinks are");
+    alcholicdrinks.forEach(logDrink);
+
+    const nonAlcholicdrinks = fancyDrinks.filter(
+      (drink) => drink.strAlcoholic != "Alcoholic"
+    );
+    console.log("Nonalcholic drinks are");
+    nonAlcholicdrinks.forEach(logDrink);
   })
-
 
   .catch((e) => console.error(e));
 
-  //parseDrink function takes a drink object and returns a new object with a id ,name and ingredients
-  const parseDrink=(drink)=>{
-    return {
-      id: drink.idDrink,
-      name:drink.strDrink,
-      ingredients: parseIngredients(drink)
-    }
-  }
+//parseDrink function takes a drink object and returns a new object with a id ,name and ingredients
+const parseDrink = (drink) => {
+  return {
+    id: drink.idDrink,
+    name: drink.strDrink,
+    ingredients: parseIngredients(drink),
+  };
+};
 // parse ingredient function takes a drink object and return an array of ingredient name
-  const parseIngredients =(drink)=>{
-    const array = []
-    let i = 1
-    while (drink['strIngredient'+ i]!= null){
-      array.push({
-        name: drink['strIngredient'+ i],
-        quantity:drink['strMeasure'+ i],
-      })
-      i++
-    }
-    return array ;
+const parseIngredients = (drink) => {
+  const array = [];
+  let i = 1;
+  while (drink["strIngredient" + i] != null) {
+    array.push({
+      name: drink["strIngredient" + i],
+      quantity: drink["strMeasure" + i],
+    });
+    i++;
   }
+  return array;
+};
 
-  const logDrink =(drink)=>{
-    console.log("Drink Id",drink.id,"is called",drink.name,"and is made with",drink.ingredients)
-
-  }
+const logDrink = (drink) => {
+  console.log(
+    "Drink Id",
+    drink.id,
+    "is called",
+    drink.name,
+    "and is made with",
+    drink.ingredients
+  );
+};
